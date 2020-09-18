308268dcc7c2cfdc9c39a5c749304b2e1c73a2e3
Kristoffer <krismad@stud.ntnu.no>
2020-09-18 14:48:12 +0200

    added consts.hpp and started working on map reading function

---
+++ src/framework/consts.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <cstring>
+namespace framework {
+	string level_0_path = "levels\level0";
+}
--- src/framework/tilemap.hpp
+++ src/framework/tilemap.hpp
@@ -1,1 +1,15 @@
 #pragma once
+namespace framework{
+
+	class Map {
+	
+	private:
+		int sizeX sizeY;
+		int array[];
+
+	public:
+		Map();
+
+	};
+
+}
