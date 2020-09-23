f71b87184ce5a8d9d0bc59b4522393da126bc384
Daniel <daniedah@stud.ntnu.no>
2020-09-23 16:11:20 +0200

    added consts for shader paths

--- src/framework/consts.hpp
+++ src/framework/consts.hpp
@@ -4,4 +4,6 @@ namespace framework {
 
 	const std::string LEVELPATH0 = "../res/levels/level0";
 
+	const std::string VERTSHADERPATH = "../res/shaders/shader.vert";
+	const std::string FRAGSHADERPATH = "../res/shaders/shader.frag";
 }
