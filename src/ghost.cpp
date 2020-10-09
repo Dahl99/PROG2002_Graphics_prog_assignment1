7e64b51989d10670931aa91926eb316b7265c11d
Daniel Dahl <daniedah@stud.ntnu.no>
2020-10-10 00:00:08 +0200

    Removed commented out code

--- src/ghost.cpp
+++ src/ghost.cpp
@@ -2,21 +2,4 @@
 
 void Ghost::UpdateSprite(const GLint& dir)
 {
-	/*switch (dir)
-	{
-	case 0:
-		pos.y += dt * mvSpeed;
-		break;
-	case 1:
-		pos.x += dt * mvSpeed;
-		break;
-	case 2:
-		pos.y -= dt * mvSpeed;
-		break;
-	case 3:
-		pos.x -= dt * mvSpeed;
-		break;
-	default:
-		break;
-	}*/
 }
